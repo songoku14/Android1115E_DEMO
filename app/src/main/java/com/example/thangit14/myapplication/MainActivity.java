62ce39544379bc1891f87fc90e73ccdc519f29dc
TrungVanTrung <ngotrungngo321@gmail.com>
2016-03-11 18:34:11 +0700

    sua activity

--- app/src/main/java/com/example/thangit14/myapplication/MainActivity.java
+++ app/src/main/java/com/example/thangit14/myapplication/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private  String  trung;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
--- app/src/main/java/com/example/thangit14/myapplication/TrungActivity.java
+++ app/src/main/java/com/example/thangit14/myapplication/TrungActivity.java
@@ -4,4 +4,5 @@ package com.example.thangit14.myapplication;
  * Created by ngo trung on 09-Mar-2016.
  */
 public class TrungActivity {
+
 }
